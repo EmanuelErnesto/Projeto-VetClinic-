1ec88e1de19aad45305f70bce34fa0c2ef796e25
EmanuelErnesto <emanuel.earego@gmail.com>
2024-10-29 21:13:24 -0300

    fix: branch name of ci github actions

--- src/Infrastructure/Routes/router.js
+++
@@ -1,8 +0,0 @@
-const petRoutes = require('./pet.routes');
-const tutorRoutes = require('./tutor.routes');
-const router      = require('express').Router();
-
-router.use('/', tutorRoutes)
-router.use('/', petRoutes)
-
-module.exports = router
