f7c4e24d9fb30eab388bf3e5e9968e59b5b3e3fc
EmanuelErnesto <emanuel.earego@gmail.com>
2024-10-29 21:01:11 -0300

    feat: add tutor joi schemas

---
+++ src/modules/tutors/infra/http/joi/schemas/UpdateTutor.schema.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+export const UpdateTutorSchema = Joi.object({
+  id: Joi.number().required().positive().min(1),
+  name: Joi.string().required().max(100).min(3),
+  email: Joi.string()
+    .required()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
+  phone: Joi.string()
+    .required()
+    .pattern(/^\d{11}$/),
+  date_of_birth: Joi.string()
+    .required()
+    .pattern(
+      /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$/,
+    ),
+  zip_code: Joi.string()
+    .required()
+    .pattern(/^\d{8}$/),
+});
